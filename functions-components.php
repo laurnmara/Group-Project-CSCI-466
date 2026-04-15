fda1ca75ccbbbc25941b95b9f5a816c28c0ffdde
Lauren Braswell <business.laurenmb@gmail.com>
2026-04-15 13:01:00 -0500

    add default code to all files

--- functions-components.php
+++ functions-components.php
@@ -1,1 +1,34 @@
+<html>
+<?php
 
+    // Username and Password for connecting to DB
+    $username='z2020678';
+    $password='2005Oct28';
+
+    // Function for drawing tables
+    function draw_table($rows) {
+        if (!$rows) { echo 'No results found!'; die(); }
+        else {
+            echo "<table border=1 cellspacing=2>";
+                echo "<tr>";
+                foreach($rows[0] as $key => $item ) {
+                    echo "<th>$key</th>";
+                }
+                echo "</tr>";
+                foreach($rows as $row) {
+                    echo "<tr>";
+                    foreach($row as $item) {
+                        echo "<td>$item</td>";
+                    }
+                    echo "</tr>";
+                }
+                echo "</table>";
+        }
+    }
+
+    //Nav Bar component
+
+
+    //Footer component
+?>
+</html>
--- home.php
+++ home.php
@@ -1,1 +1,27 @@
+<html>
+    <head>
+        <title>Default</title>
+    </head>
+
+<body>
+<?php
+        // Login Credentials + Functions for DB
+        include("functions-components.php");
+
+        try {
+            // Connecting using MySql (MariaDB)
+            $dsn = "mysql:host=courses;dbname=z2020678";
+            $pdo = new PDO($dsn, $username, $password);
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+            
+        }
+        catch(PDOexception $e) {
+            echo "Connection to database has failed: " . $e->getMessage();
+        }
+?>
+
+</body>
+
+</html>
 
